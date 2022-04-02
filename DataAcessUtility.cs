0482e9c5e343db2e20436ce161104e3ad10ba770
monia7dev <101042775+monia7dev@users.noreply.github.com>
2022-04-02 18:08:41 +0100

    Add files via upload

---
+++ DataAcessUtility.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using Microsoft.Data.Sqlite;
+
+
+
+namespace Coding_Sessions_Tracker
+{
+	public class DataAcessUtility
+    {
+		
+		
+        public static SqliteConnection GetConnection(string name)
+        {
+
+            string connectionString = ConfigurationManager.ConnectionStrings[name].ConnectionString;
+
+            SqliteConnection connection = new SqliteConnection(connectionString);
+            connection.Open();
+            return connection;
+
+        }
+
+
+
+    }
+}
+
